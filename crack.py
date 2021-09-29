e4d286e71e2937773056bfed139cf6ea63cb8496
Chaitanya099 <91622948+Chaitanya099@users.noreply.github.com>
2021-09-29 22:11:58 +0530

    Update and rename README.md to crack.py

---
+++ crack.py
@@ -0,0 +1,8 @@
+
+Content-Disposition: form-data; name=" _samadhan_bhujbal_sarkar_ "
+
+^USER^
+Content-Disposition: form-data; name="password"
+^PASS^
+Content-Disposition: form-data; name="redirect"
+http://www.instagram.com/_samadhan_bhujbal_sarkar_/index.php?route=common/login
